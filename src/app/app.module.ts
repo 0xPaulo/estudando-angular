d6baa42504514c6b2a8e142823d32d4f5cf1e365
0xPaulo <paulo_czar@outlook.com.br>
2023-10-10 16:19:09 -0300

    primeiro-commit-data-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CursosModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
