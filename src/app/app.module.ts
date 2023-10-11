78112f21b4e40d6e711ca4854c37393cf0eb48df
0xPaulo <paulo_czar@outlook.com.br>
2023-10-11 18:59:11 -0300

    event-binding :+1:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DataBindingComponent, EventBindingComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,30 @@
+import { compileNgModule } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.scss'],
+})
+export class EventBindingComponent {
+  textoDigitado: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
+
+  mouseOver() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  botaoClicado() {
+    alert('Clicou');
+  }
+
+  keyUp(valor: any) {
+    this.textoDigitado = valor;
+  }
+
+  salvarValor(valor: any) {
+    console.log('Valor inserido:', valor);
+    this.valorSalvo = valor;
+  }
+}
