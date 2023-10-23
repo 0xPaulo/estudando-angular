b3ed05e9a32d1cbdad3c33ebb2fd7feeeb289a01
0xPaulo <paulo_czar@outlook.com.br>
2023-10-23 18:20:09 -0300

    libs-cli-iniciada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { BindingModule } from './data-binding-module/binding.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BindingModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
