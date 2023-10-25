2655e112ee45124cb170b1df2c4f3650b427d79a
0xPaulo <paulo_czar@outlook.com.br>
2023-10-25 16:49:37 -0300

    ng-for :+1:

--- src/app/diretivas/diretivas.module.ts
+++ src/app/diretivas/diretivas.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DiretivasComponent } from './diretivas.component';
 import { DiretivasNgIfComponent } from './diretivas-ng-if/diretivas-ng-if.component';
 import { DiretivasNgSwitchComponent } from './diretivas-ng-switch/diretivas-ng-switch.component';
+import { DiretivasNgForComponent } from './diretivas-ng-for/diretivas-ng-for.component';
 
 @NgModule({
   declarations: [
     DiretivasComponent,
     DiretivasNgIfComponent,
     DiretivasNgSwitchComponent,
+    DiretivasNgForComponent,
   ],
   exports: [DiretivasComponent],
   imports: [CommonModule],
